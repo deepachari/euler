8f0ec3ba2abda9548618f4e57587ffb629e52812
deepachari <dchari@newclassrooms.org>
2017-10-26 18:37:42 -0400

    Problem 6 - easy list comprehension

---
+++ problem6.py
@@ -0,0 +1,17 @@
+# The sum of the squares of the first ten natural numbers is,
+#
+# 12 + 22 + ... + 102 = 385
+# The square of the sum of the first ten natural numbers is,
+#
+# (1 + 2 + ... + 10)**2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+# 3025 - 385 = 2640.
+#
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+def problem6(x):
+    return sum(range(x+1))**2 - sum(i**2 for i in range(x+1))
+
+
+print problem6(100)
