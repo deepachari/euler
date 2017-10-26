b28fb8dfdf41b87f3ec38added8d4990d8f0e9d3
deepachari <dchari@newclassrooms.org>
2017-10-26 18:24:22 -0400

    Problem 5 - brute force

---
+++ problem5.py
@@ -0,0 +1,18 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+# brute force
+def found(x):
+    for i in range(1, 20):
+        if x % i != 0:
+            return False
+
+    return True
+
+result = 40
+while not found(result):
+    result += 20
+
+print result
